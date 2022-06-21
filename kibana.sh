af7d9807efed78ed9035d4ee6acd9fefd27519a9
Abhishek Dubey <45308951+dubeyji10@users.noreply.github.com>
2022-06-21 18:18:44 +0530

    Add files via upload
    script to set path and run kibana

---
+++ kibana.sh
@@ -0,0 +1,2 @@
+echo "starting kibana"
+gnome-terminal --tab --title="kibana" --command="bash -c 'cd;cd /home/abhishek/Downloads/kibana-7.17.4-linux-x86_64;echo running kibana in second tab; ./bin/kibana ;$SHELL'"
